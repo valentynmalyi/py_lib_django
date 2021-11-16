611f6d85bbfcb331bb7f41066be971e50c97bc7b
valentynmalyi <valentynmalyi@gmail.com>
2021-11-16 16:43:54 +0200

    add divided by 3

--- pylib_libs/divided_by_2_3/utils.py
+++ pylib_libs/divided_by_2_3/utils.py
@@ -2,3 +2,15 @@ def is_divided_by_2(n: int) -> bool:
     if n % 2 == 0:
         return True
     return False
+
+
+def is_divided_by_3(n: int) -> bool:
+    if n % 3 == 0:
+        return True
+    return False
+
+
+def is_divided(n: int) -> bool:
+    if is_divided_by_2(n):
+        return True
+    return is_divided_by_3(n)
