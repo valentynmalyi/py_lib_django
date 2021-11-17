8977c182f8ebf4dce7735ac0c9e3aed24e3ca00f
valentynmalyi <valentynmalyi@gmail.com>
2021-11-17 12:14:48 +0200

    good place for validator

--- apps/prime/serializers.py
+++ apps/prime/serializers.py
@@ -6,15 +6,12 @@ from apps.prime import validators
 class PrimeNumberSerializer(serializers.Serializer):
     n = serializers.IntegerField(min_value=2, max_value=100)
 
-    class Meta:
-        extra_kwargs = {
-            "n": {
-                "validators": (validators.is_prime_validate,)
-            }
-        }
-
     def create(self, validated_data):
         pass
 
     def update(self, instance, validated_data):
         pass
+
+    # noinspection PyMethodMayBeStatic
+    def validate_n(self, n: int):
+        validators.is_prime_validate(n)
