b4b4cb4d75956fa88f56f79f0e1e090268abe48c
valentynmalyi <valentynmalyi@gmail.com>
2021-11-17 11:19:16 +0200

    add div5_7 exeption

--- apps/prime/views.py
+++ apps/prime/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework import views, status
 
 from apps.prime import serializers
-from pylib_libs import divided_by_2_3
+from pylib_libs import divided_by_2_3, divided_by_5_7
 
 
 class PrimeNumber(views.APIView):
@@ -11,7 +11,7 @@ class PrimeNumber(views.APIView):
         serializer = serializers.PrimeNumberSerializer(data={"n": n})
         try:
             serializer.is_valid(raise_exception=True)
-        except divided_by_2_3.exceptions.DividedException as e:
+        except (divided_by_2_3.exceptions.DividedException, divided_by_5_7.exceptions.DividedException) as e:
             return Response(data={"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(data={"status": "ok"})
