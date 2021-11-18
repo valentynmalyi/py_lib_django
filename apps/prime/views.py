2e7c692e2d145eabba6e94d1108dba4b8f56a132
valentynmalyi <valentynmalyi@gmail.com>
2021-11-18 17:47:56 +0200

    rm imports

--- apps/prime/views.py
+++ apps/prime/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
-from rest_framework import views, status
+from rest_framework import views
 
-from apps.prime import serializers, exceptions
+from apps.prime import serializers
 
 
 class PrimeNumber(views.APIView):
