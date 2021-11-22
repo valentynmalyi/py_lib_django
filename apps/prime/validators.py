2a3af6be1d8a42d321b8ac95f1426f80a4e60614
valentynmalyi <valentynmalyi@gmail.com>
2021-11-22 10:50:45 +0200

    success

--- apps/prime/validators.py
+++ apps/prime/validators.py
@@ -1,6 +1,12 @@
+from rest_framework.exceptions import ValidationError
+
 from pylib_libs import divided_by_2_3, divided_by_5_7
+from apps.prime import exceptions
 
 
 def is_prime_validate(n: int):
-    divided_by_2_3.utils.validate(n)
-    divided_by_5_7.utils.validate(n)
+    try:
+        divided_by_2_3.utils.validate(n)
+        divided_by_5_7.utils.validate(n)
+    except exceptions.DividedException as e:
+        raise ValidationError(str(e)) from e
--- pylib/settings.py
+++ pylib/settings.py
@@ -50,8 +50,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'apps.prime.middlewares.SimpleMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'pylib.urls'
